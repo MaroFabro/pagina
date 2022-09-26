5808c2c4cccdeb2e0c7c12830a627130247b044c
MaroFabro <113359924+MaroFabro@users.noreply.github.com>
2022-09-26 11:13:54 -0300

    Add files via upload

--- clic/artistas/forms.py
+++ clic/artistas/forms.py
@@ -1,18 +1,27 @@
 from django import forms
 from typing_extensions import Required
 class Inscripción(forms.Form):
+    from django import forms
 
-    Nombre_de_banda_o_artista=forms.CharField(max_length=30)
-    Cantidad_de_integrantes=forms.IntegerField()
-    Infromación_artística_o_reseña=forms.CharField(max_length=255)
-    Rubro_o_género_de_música=forms.CharField(max_length=100)
-    Provincia=forms.CharField(max_length=40)
-    Localidad=forms.CharField(max_length=40)
-    Contacto_telefónico=forms.CharField(max_length=15)
-    Contacto_Mail=forms.EmailField()
-    Link_o_sitios_de_material_discográfico=forms.CharField(max_length=1000)
-    Razón_social=forms.CharField(max_length=255)
-    Costos=forms.IntegerField()
+provincia= [
+    ("Buenos Aires")
+]
+
+localidad= [
+    ("Baradero")
+]
+
+Nombre_de_banda_o_artista=forms.CharField(max_length=30)
+Cantidad_de_integrantes=forms.IntegerField()
+Infromación_artística_o_reseña=forms.CharField(max_length=255)
+Rubro_o_género_de_música=forms.CharField(max_length=100)
+Provincia=forms.CharField(label="Seleccione su provincia", widget=forms.Select(choices=provincia))
+Localidad=forms.CharField(label="Seleccione su localidad", widget=forms.Select(choices=localidad))
+Contacto_telefónico=forms.CharField(max_length=15)
+Contacto_Mail=forms.EmailField()
+Link_o_sitios_de_material_discográfico=forms.CharField(max_length=1000)
+Razón_social=forms.CharField(max_length=255)
+Costos=forms.IntegerField()
 
 class FormularioContacto(forms.Form):
 
--- clic/artistas/models.py
+++ clic/artistas/models.py
@@ -1,17 +1,18 @@
-
+from django import forms
 from typing_extensions import Required
 from django.db import models
 
 # Create your models here.
 
 class Inscripcion(models.Model):
-    Nombre_de_banda_o_artista=models.CharField(max_length=30), Required
+    Nombre_de_banda_o_artista=models.CharField (max_length=30), (Required)
     Cantidad_de_integrantes=models.IntegerField()
-    Infromación_artística_o_reseña=models.CharField(max_length=255), Required
-    Rubro_o_género_de_música=models.CharField(max_length=100), Required
-    Localidad=models.CharField(max_length=100)
-    Contacto_telefónico=models.CharField(max_length=15), Required
-    Contacto_Mail=models.EmailField(), Required
+    Infromación_artística_o_reseña=models.CharField(max_length=255), (Required)
+    Rubro_o_género_de_música=models.CharField(max_length=100), (Required)
+    Provincia=models.CharField(max_length=50)
+    Localidad=models.CharField(max_length=50)
+    Contacto_telefónico=models.CharField(max_length=15), (Required)
+    Contacto_Mail=models.EmailField(Required)
     Link_o_sitios_de_material_discográfico=models.CharField(max_length=1000)
     Razón_social=models.CharField(max_length=255)
-    Costos=models.IntegerField(), Required
+    Costos=models.IntegerField(Required)
